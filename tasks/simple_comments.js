403b6642666dd4be5031c4b7a2bc1b15c6122408
Tom Najdek <tomek@potatolondon.com>
2014-01-18 11:22:20 +0000

    Initial grunt plugin layout

---
+++ tasks/simple_comments.js
@@ -0,0 +1,50 @@
+/*
+ * grunt-simple-comments
+ * https://github.com/tnajdek/grunt-simple-comments
+ *
+ * Copyright (c) 2014 Tom Najdek
+ * Licensed under the MIT license.
+ */
+
+'use strict';
+
+module.exports = function(grunt) {
+
+  // Please see the Grunt documentation for more information regarding task
+  // creation: http://gruntjs.com/creating-tasks
+
+  grunt.registerMultiTask('simple_comments', 'Grunt task to setup and remotely review comments stored on parse.com and created on a site using simple-comments library', function() {
+    // Merge task-specific and/or target-specific options with these defaults.
+    var options = this.options({
+      punctuation: '.',
+      separator: ', '
+    });
+
+    // Iterate over all specified file groups.
+    this.files.forEach(function(f) {
+      // Concat specified files.
+      var src = f.src.filter(function(filepath) {
+        // Warn on and remove invalid source files (if nonull was set).
+        if (!grunt.file.exists(filepath)) {
+          grunt.log.warn('Source file "' + filepath + '" not found.');
+          return false;
+        } else {
+          return true;
+        }
+      }).map(function(filepath) {
+        // Read file source.
+        return grunt.file.read(filepath);
+      }).join(grunt.util.normalizelf(options.separator));
+
+      // Handle options.
+      src += options.punctuation;
+
+      // Write the destination file.
+      grunt.file.write(f.dest, src);
+
+      // Print a success message.
+      grunt.log.writeln('File "' + f.dest + '" created.');
+    });
+  });
+
+};
